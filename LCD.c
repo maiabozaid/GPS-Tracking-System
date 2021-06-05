b74c7c8b176f843eb8e0c61f97a011a3762c1784
maiabozaid <77507104+maiabozaid@users.noreply.github.com>
2021-06-05 22:58:33 +0200

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -19,11 +19,11 @@ void LCD_command(char x) {
 }
 
 void LCD_DATA(char y) {
-	GPIO_PORTC_DATA_R = 0X20;	 // Rs =1 , Rw &E = 0 
+	GPIO_PORTC_DATA_R = 0X20;	 // Rs =1 , Rw &E = 0 //00100000 =0x20
 	GPIO_PORTB_DATA_R = y;
-	GPIO_PORTC_DATA_R |= 0x80; //ENABLING
+	GPIO_PORTC_DATA_R |= 0x80; //ENABLING 
 	delay(50);
-	GPIO_PORTC_DATA_R &= ~0x80;
+	GPIO_PORTC_DATA_R &= ~0x80;  //enable: from 1 to 0
 	LCD_command(0x06);	//Increment cursor
 }
 
@@ -52,10 +52,10 @@ void intial_LCD(void)
 	GPIO_PORTB_PCTL_R = 0x00000000;
 	GPIO_PORTB_DEN_R = 0xFF; 
 	GPIO_PORTB_DIR_R = 0xFF; // all are output
-	//GPIO_PORTB_DATA_R = 'H';
+	
 
 									 // PORT C
-	GPIO_PORTC_LOCK_R = 0x4C4F434B;    // rs : bin 0 ,rw: bin 1 ,enable : bin 2  output  //101
+	GPIO_PORTC_LOCK_R = 0x4C4F434B;    
 	GPIO_PORTC_CR_R |= 0xE0;	// Although they are already = 1 but just to make the code general
 	GPIO_PORTC_AMSEL_R &= ~0xE0;
 	GPIO_PORTC_AFSEL_R &= ~0xE0;
