f9686cfdc07d4274defee54965f15be078344e0f
David-Samir-Luis <85419503+David-Samir-Luis@users.noreply.github.com>
2021-06-06 15:28:49 +0200

    Update UART0_Init.c

--- UART0_Init.c
+++ UART0_Init.c
@@ -10,7 +10,7 @@ UART0_FBRD_R       = 0x0035    ;
 UART0_LCRH_R       = 0x0070    ;        // Enable FIFO and 8-bit word lengh
 UART0_CTL_R        = 0x0301    ;        // Enable UART , RXE and TXE 
 GPIO_PORTA_AFSEL_R|= 0x03      ;        // Enable alternate function for PAO and PA1
-GPIO_PORTA_PCTL_R  =(GPIO_PORTA_PCTL_R & ~0x000000ff)|0x00000011;  // clear the previous 8-bits corresponding to PAO and PA1 .set the bits that enable UART0
+GPIO_PORTA_PCTL_R  =(GPIO_PORTA_PCTL_R & ~0x000000ff)|0x00000011;  // clear the old 8-bits corresponding to PAO and PA1 .set the bits that enable UART0
 GPIO_PORTA_DEN_R  |= 0x03      ;        // Enable digital for PAO and PA1 
 GPIO_PORTA_AMSEL_R&= 0x03      ;        // disable analog for PAO and PA1 
 }
