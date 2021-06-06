e8ec242edab33f3285e5b1700fbdb48d58418d37
peter-samir-luis <68796155+peter-samir-luis@users.noreply.github.com>
2021-06-06 21:20:06 +0200

    Add files via upload

---
+++ GPS_data.c
@@ -0,0 +1,14 @@
+#include "D:/2nd computer/2nd term/Embedded Systems/tm4c123gh6pm.h"
+#include "stdint.h"
+
+uint8_t UART0_InChar()
+{
+ while ((UART0_FR_R & 0x0010) != 0);        // wait until RXFE isn't empty 
+ return ((uint8_t)(UART0_DR_R & 0xff));     // Read UART0 data ,cast to integer
+}
+
+
+int main()
+{
+
+}
