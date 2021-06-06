ddab20a9b7f378770b8260e07cc5a3bc03f3db9f
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2021-06-06 06:51:33 -0700

    Update function_cal_total_distance.h

--- function_cal_total_distance.h
+++ function_cal_total_distance.h
@@ -1,6 +1,8 @@
 #include "stdint"
 #include <math.h>
-#define  pi 3.14159265358979323846
+#define  pi 3.14159265358979323846        
+
+//constant pi which equal 22/7
 
 
 
@@ -8,6 +10,7 @@
 
 /******************************************/
 
-float convert_deg2rad(float degree);
-float convert_rad2deg(float radian);
-float cal_distance(float latitude_1, float longitude_1, float latitude_2, float longitude_2);
+float convert_deg2rad(float degree);                                                   // function which converts from degree to radian 
+float convert_rad2deg(float radian);                                                  // function which converts from radian  to degree
+float cal_distance(float latitude_1, float longitude_1, float latitude_2, float longitude_2);  
+
