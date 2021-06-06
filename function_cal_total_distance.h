9d6865ba8efbd493e6d920e2212758a286c98618
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2021-06-06 13:13:06 +0200

    Add files via upload

---
+++ function_cal_total_distance.h
@@ -0,0 +1,13 @@
+#include "stdint"
+#include <math.h>
+#define  pi 3.14159265358979323846
+
+
+
+
+
+/*****************************************/
+
+float convert_deg2rad(float degree);
+float convert_rad2deg(float radian);
+float cal_distance(float latitude_1, float longitude_1, float latitude_2, float longitude_2);
