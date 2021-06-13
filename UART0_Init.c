95f30e64def48d70731f21588565018f84de8c3d
David-Samir-Luis <85419503+David-Samir-Luis@users.noreply.github.com>
2021-06-13 20:07:55 +0200

    Update UART0_Init.c

--- UART0_Init.c
+++ UART0_Init.c
@@ -1,16 +1,16 @@
-#include "tm4c123gh6pm.h"
+#include "D:/2nd computer/2nd term/Embedded Systems/Project/tm4c123gh6pm.h"
 
-void A_UART0_Init()
-{
-SYSCTL_RCGCUART_R  |= 0x0001    ;        // Activate UART0
-SYSCTL_RCGCGPIO_R  |= 0x0001    ;        // Activate port A
-UART0_CTL_R        &= ~0x0001   ;        // disable UART0 (to avoid sending data during Configuration)
-UART0_IBRD_R        = 0x0208    ;        // clock = 80 MHz , Buad rate = 9600 ==> d = (80*10^8/(16*9600)) = 520.8333 ==> IBRD = 520
-UART0_FBRD_R        = 0x0035    ;        // FBRD = (0.8333*64) + 0.5 = 53
-UART0_LCRH_R        = 0x0070    ;        // Enable FIFO and 8-bit word lengh
-UART0_CTL_R         = 0x0301    ;        // Enable UART , RXE and TXE 
-GPIO_PORTA_AFSEL_R |= 0x03      ;        // Enable alternate function for PAO and PA1
-GPIO_PORTA_PCTL_R   = (GPIO_PORTA_PCTL_R & ~0x000000ff)|0x00000011;  // clear the old 8-bits corresponding to PAO and PA1 .set the bits that enable UART0
-GPIO_PORTA_DEN_R   |= 0x03      ;        // Enable digital for PAO and PA1 
-GPIO_PORTA_AMSEL_R &= 0x03      ;        // disable analog for PAO and PA1 
+void UART5_INIT(){
+SYSCTL_RCGCUART_R |= 0x20 ;       //0000 0000   PORT E
+SYSCTL_RCGCGPIO_R |= 0x10 ;
+UART5_CTL_R &= ~0x01;
+UART5_IBRD_R = 104;            // System Clock = 16 MHZ    by default
+UART5_FBRD_R = 11;
+UART5_LCRH_R |= 0x70  ;
+UART5_CTL_R = 0x0301;
+  
+GPIO_PORTE_AFSEL_R |= 0x30;
+GPIO_PORTE_PCTL_R = (GPIO_PORTE_PCTL_R &= ~0x00ff0000)|(0x00110000);
+GPIO_PORTE_DEN_R |= 0x30;
+GPIO_PORTE_AMSEL_R &= ~0x30;  
 }
