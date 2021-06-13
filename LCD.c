0789210b3c232a384b7e8f62cb816f06803bf98e
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2021-06-13 14:07:17 -0700

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -58,7 +58,44 @@ void LCD_number(int x) {
      LCD_command(0x06);
 
 }
+void reverse(char* str, int len)
+{
+    int i = 0, j = len - 1, temp;
+    while (i < j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+}
+int integerToString(int x, char str[], int d)
+{
+    int i = 0;
+    while (x) {
+        str[i++] = (x % 10) + '0';
+        x = x / 10;
+    }
+    while (i < d)
+        str[i++] = '0';
 
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
+void ftoa(float n, char* res, int afterpoint)
+{
+    int ipart = (int)n;
+    float fpart = n - (float)ipart;
+    int i = integerToString(ipart, res, 0);
+    if (afterpoint != 0) {
+        res[i] = '.'; 
+
+        fpart = fpart * pow(10, afterpoint);
+
+        integerToString((int)fpart, res + i + 1, afterpoint);
+    }
+}
 
 void intial_LCD(void)
 {
