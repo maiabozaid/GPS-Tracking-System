e67b729085abb91bdb49cce927194aa34ae208a7
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2021-06-13 12:35:05 -0700

    Create final_cal_total_distance.h

---
+++ final_cal_total_distance.h
@@ -0,0 +1,9 @@
+#include "stdint"
+#include <math.h>
+#define  pi 3.14159265358979323846        
+
+//constant pi which is equal 22/7
+
+float convert_deg2rad(float degree);                                                   // function which converts from degree to radian 
+float convert_rad2deg(float radian);                                                  // function which converts from radian  to degree
+float cal_distance(float latitude_1, float longitude_1, float latitude_2, float longitude_2);  
