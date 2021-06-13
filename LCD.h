f5c38cbcfcb70268c8f9ad29d353164d0ee77613
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2021-06-13 14:11:49 -0700

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -6,3 +6,6 @@ void LCD_DATA(char y);
 void LCD_display_string(char* mystring);
 void LCD_number(int x) ;
 void intial_LCD(void);
+void reverse(char* str, int len);
+int integerToString(int x, char str[], int d);
+void ftoa(float n, char* res, int afterpoint);
